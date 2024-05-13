9a6ed818c1d8432065e6a7e5f61eace9482c744d
Korsakov <kors2222@yandex.ru>
2024-05-13 19:11:43 +0300

    Get started web MVC

---
+++ src/main/java/ru/kors/webmvc/controller/HelloWorldController.java
@@ -0,0 +1,14 @@
+package ru.kors.webmvc.controller;
+
+import org.springframework.boot.SpringBootVersion;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/hello")
+    public String hello() {
+        String version = SpringBootVersion.getVersion();
+        return String.format("Hello World! Spring Boot version %s", version);
+    }
+}
---
+++ src/test/java/ru/kors/webmvc/controller/HelloWorldControllerTest.java
@@ -0,0 +1,27 @@
+package ru.kors.webmvc.controller;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void shouldReturnStringFromHello() throws Exception {
+        String expected = "Hello World! Spring Boot version 3.2.5";
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString(expected)))
+                .andExpect(content().contentTypeCompatibleWith(MediaType.TEXT_PLAIN));
+    }
+}
